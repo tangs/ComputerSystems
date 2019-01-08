7d13a9120b22e6365fdf0d8567e7dd47bffbb669
tangs <tangsong1988kaka@gmail.com>
2019-01-08 10:38:42 +0800

    Update.

--- 3/main.c
+++ 3/main.c
@@ -2,10 +2,24 @@
 
 void multstore(long, long, long *);
 
+void uchar2long(unsigned char* v1, long* v2) {
+    *v2 = (long)(*v1);
+}
+
 int main() {
-    long d;
-    multstore(2, 3, &d);
-    printf("2 * 3 --> %ld\n", d);
+    // long d;
+    // multstore(2, 3, &d);
+    // printf("2 * 3 --> %ld\n", d);
+    unsigned char v1 = 0x00;
+    long v2 = 0;
+    uchar2long(&v1, &v2);
+    printf("%ld\n", v2);
+    v1 = 0x80;
+    uchar2long(&v1, &v2);
+    printf("%ld\n", v2);
+    v1 = 0xFF;
+    uchar2long(&v1, &v2);
+    printf("%ld\n", v2);
     return 0;
 }
 
--- 3/main.s
+++ 3/main.s
@@ -1,5 +1,21 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 10, 14
+	.globl	_uchar2long             ## -- Begin function uchar2long
+	.p2align	4, 0x90
+_uchar2long:                            ## @uchar2long
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movzbl	(%rdi), %eax
+	movq	%rax, (%rsi)
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
 	.globl	_main                   ## -- Begin function main
 	.p2align	4, 0x90
 _main:                                  ## @main
@@ -10,17 +26,46 @@ _main:                                  ## @main
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	leaq	-8(%rbp), %rdx
-	movl	$2, %edi
-	movl	$3, %esi
-	callq	_multstore
-	movq	-8(%rbp), %rsi
-	leaq	L_.str(%rip), %rdi
+	pushq	%r15
+	pushq	%r14
+	pushq	%rbx
+	subq	$24, %rsp
+	.cfi_offset %rbx, -40
+	.cfi_offset %r14, -32
+	.cfi_offset %r15, -24
+	movb	$0, -25(%rbp)
+	movq	$0, -40(%rbp)
+	leaq	-25(%rbp), %rbx
+	leaq	-40(%rbp), %r14
+	movq	%rbx, %rdi
+	movq	%r14, %rsi
+	callq	_uchar2long
+	movq	-40(%rbp), %rsi
+	leaq	L_.str(%rip), %r15
 	xorl	%eax, %eax
+	movq	%r15, %rdi
+	callq	_printf
+	movb	$-128, -25(%rbp)
+	movq	%rbx, %rdi
+	movq	%r14, %rsi
+	callq	_uchar2long
+	movq	-40(%rbp), %rsi
+	xorl	%eax, %eax
+	movq	%r15, %rdi
+	callq	_printf
+	movb	$-1, -25(%rbp)
+	movq	%rbx, %rdi
+	movq	%r14, %rsi
+	callq	_uchar2long
+	movq	-40(%rbp), %rsi
+	xorl	%eax, %eax
+	movq	%r15, %rdi
 	callq	_printf
 	xorl	%eax, %eax
-	addq	$16, %rsp
+	addq	$24, %rsp
+	popq	%rbx
+	popq	%r14
+	popq	%r15
 	popq	%rbp
 	retq
 	.cfi_endproc
@@ -43,7 +88,7 @@ _mult2:                                 ## @mult2
                                         ## -- End function
 	.section	__TEXT,__cstring,cstring_literals
 L_.str:                                 ## @.str
-	.asciz	"2 * 3 --> %ld\n"
+	.asciz	"%ld\n"
 
 
 .subsections_via_symbols
