e0b5df8ab9b8d3757dce9a802a972acfc156628c
tangs <tangsong1988kaka@gmail.com>
2019-01-14 20:57:04 +0800

    Update.

--- 3/3.22/main.c
+++ 3/3.22/main.c
@@ -6,7 +6,7 @@ long get_max();
 
 int main() {
     long num = 1;
-    for (long i = 1; i < 30; ++i) {
+    for (long i = 0; i < 30; ++i) {
         printf("%ld: %ld, %d\n", i, fact_do(i), is_mut_overflow(i, num));
         num *= i;
     }
