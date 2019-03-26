dc7d3bdefcef7e5195aeb79427fe4e269c976ad0
tangs <tangsong1988kaka@gmail.com>
2019-03-26 17:18:36 +0800

    update.

--- 3/tmp/6.c
+++ 3/tmp/6.c
@@ -1,3 +1,7 @@
 double cvt(float f) {
     return (double)f;
 }
+
+double funct(double a, float x, double b, int i) {
+    return a*x - b/i;
+}
--- 3/tmp/6.s
+++ 3/tmp/6.s
@@ -10,7 +10,26 @@ _cvt:                                   ## @cvt
 	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register %rbp
-	cvtss2sd	%xmm0, %xmm0
+	vcvtss2sd	%xmm0, %xmm0, %xmm0
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.globl	_funct                  ## -- Begin function funct
+	.p2align	4, 0x90
+_funct:                                 ## @funct
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	vcvtss2sd	%xmm1, %xmm1, %xmm1
+	vmulsd	%xmm0, %xmm1, %xmm0
+	vcvtsi2sdl	%edi, %xmm3, %xmm1
+	vdivsd	%xmm1, %xmm2, %xmm1
+	vsubsd	%xmm1, %xmm0, %xmm0
 	popq	%rbp
 	retq
 	.cfi_endproc
